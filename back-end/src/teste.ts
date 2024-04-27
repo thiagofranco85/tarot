52c34d0781fc8f2aacfef79edff7fd057ba334c1
thiagofranco85 <thiago.boo@gmail.com>
2024-04-26 22:27:59 -0300

    Criando um amostra no arquivo teste.ts

--- back-end/src/teste.ts
+++ back-end/src/teste.ts
@@ -1,8 +1,35 @@
-import dotenv from "dotenv";
-import path from "path";
+import { Lennormand } from "./entities/Lennormand";
+import { MajorArcana } from "./entities/Tarot/MajorArcana";
+import { MinorArcana } from "./entities/Tarot/MinorArcana";
+import { Tarot } from "./entities/Tarot/Tarot";
+import { LennormandCards } from "./entities/Types/LennormandCards";
+import { Rank } from "./entities/Types/Rank";
+import { Suit } from "./entities/Types/Suit";
+import { Trump } from "./entities/Types/Trump";
+import { IAGemini } from "./libs/IAGemini";
+/*
+const lennormand1 = new Lennormand(null, LennormandCards.Rider);
+const lennormand2 = new Lennormand(null, LennormandCards.Mountain);
 
-console.log(path.resolve(__dirname, "../.env"));
+const pergunta = `Fiz uma pergunta ao baralho cigano. A pergunta foi: 
+Como será meu passeio no parque amanhã?
+A combinaçao de cartas que saiu foi: ${lennormand1.name} e ${lennormand2.name}.
+Qual é a resposta do baralho cigano?`;
+*/
 
-dotenv.config({ path: path.resolve(__dirname, "../.env") });
+const tarot1 = new Tarot(undefined, new MajorArcana(null, Trump.Death));
+const tarot2 = new Tarot(new MinorArcana(null, Suit.Spades, Rank.Nine));
 
-console.log(process.env.AI_GEMINI_API_KEY);
+const pergunta = `Fiz uma pergunta ao Tarot. A pergunta foi: 
+Como será meu passeio no parque amanhã?
+A pergunta foi respondida com combinaçao de 1 arcano maior e 1 arcano menor, sendo que o menor complementa o sentido do maior:
+${tarot1.majorArcana?.fullName} e ${tarot2.minorArcana?.fullName}.
+Qual é a resposta do Tarot?`;
+
+console.log(pergunta);
+
+(async function () {
+  const r = new IAGemini(pergunta);
+  const resp = await r.sendRequest();
+  console.log(resp);
+})();
